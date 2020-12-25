58049512f2796fa957828ecc064ccdf924ec986d
Carlo Schneider <contact@carloschneider.dev>
2020-12-24 22:18:25 -0300

    chore: change js files to typescript extension

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+const HomePage = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+
+    <h1>Hello world</h1>
+  </div>
+)
+
+export default HomePage
