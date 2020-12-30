3fe2dc209a86b300bfc6a5af4120a2effc4dc743
Carlo Schneider <contact@carloschneider.dev>
2020-12-29 23:23:02 -0300

    chore: ignore pages, stories and styles directories from coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**/*.tsx',
+    '!src/styles/**/*.ts',
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 }
